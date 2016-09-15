29ac90cc9266b706a678fd92ad62e651519bc629
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-09-15 15:18:47 -0500

    Completed the demos for module 3: All about joins.

--- Demos/Demo3.sql
+++ Demos/Demo3.sql
@@ -35,3 +35,31 @@ ON od.SalesOrderID = oh.SalesOrderID
 INNER JOIN SalesLT.ProductCategory AS cat
 ON p.ProductCategoryID = cat.ProductCategoryID
 ORDER BY p.ProductID; 
+
+-- Create employee table so we can try out self join
+CREATE TABLE SalesLT.Employee
+(EmployeeID int IDENTITY PRIMARY KEY,
+EmployeeName nvarchar(256),
+ManagerID int);
+GO
+-- Populate the table using data from existing table
+INSERT INTO SalesLT.Employee (EmployeeName, ManagerID)
+SELECT DISTINCT Salesperson, NULLIF(CAST(RIGHT(SalesPerson, 1) as INT), 0)
+FROM SalesLT.Customer;
+GO
+SELECT * FROM SalesLT.Employee;
+UPDATE SalesLT.Employee
+SET ManagerID = (SELECT MIN(EmployeeID) FROM SalesLT.Employee WHERE ManagerID IS NULL)
+WHERE ManagerID IS NULL
+AND EmployeeID > (SELECT MIN(EmployeeID) FROM SalesLT.Employee WHERE ManagerID IS NULL);
+GO
+-- Now, who reports to whom?
+-- Use a self-join. A self-join is an inner, outer or cross join that matches rows in a table
+-- to othe rows in the same table.
+-- When defining a self-join, you mush specify an alias for at least one instance of
+-- the table bing joined.
+SELECT e.EmployeeName, m.EmployeeName AS ManagerName
+FROM SalesLT.Employee AS e
+LEFT JOIN SalesLT.Employee AS m
+ON e.ManagerID = m.EmployeeID
+ORDER BY e.ManagerID;
