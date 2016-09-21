bb225bf346ac21530a28fecedf7f632f0a187c25
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-09-21 12:45:11 -0500

    Add Lab 5-3.

---
+++ Labs/Lab5-3.sql
@@ -0,0 +1,31 @@
+-- Challenge 3: Aggregate Product Sales
+
+-- 1. Retrieve total sales by product
+
+-- product names
+-- total revenue
+-- as sum of LineTotal
+-- sort in desc order of total revenue
+
+SELECT  Name, SUM(LineTotal) AS TotalRevenue
+FROM SalesLT.SalesOrderDetail AS od
+JOIN SalesLT.Product AS p ON od.ProductID=p.ProductID
+GROUP BY p.Name
+ORDER BY TotalRevenue DESC;
+
+-- 2. Filter the product sales list to include only products that cost over $1000
+SELECT  Name, SUM(LineTotal) AS TotalRevenue
+FROM SalesLT.SalesOrderDetail AS od
+JOIN SalesLT.Product AS p ON od.ProductID=p.ProductID
+WHERE p.ListPrice > 1000
+GROUP BY p.Name
+ORDER BY TotalRevenue DESC;
+
+-- 3. Filter the product sales groups to include only totalsales over $20,000
+SELECT  Name, SUM(LineTotal) AS TotalRevenue
+FROM SalesLT.SalesOrderDetail AS od
+JOIN SalesLT.Product AS p ON od.ProductID=p.ProductID
+WHERE p.ListPrice > 1000
+GROUP BY p.Name
+HAVING SUM(LineTotal) > 20000
+ORDER BY TotalRevenue DESC;
